4ed9c6bad111f2e2e5da46c26402e2b22b13aeab
Steven Bock <steven.bockster@gmail.com>
2015-03-16 18:13:07 -0700

    Resetting Particles.js settings

--- js/my.js
+++ js/my.js
@@ -12,12 +12,12 @@ particlesJS('particles-js', {
         sync: false
       }
     },
-    size: 2.5,
+    size: 4,
     size_random: true,
-    nb: 100,
+    nb: 150,
     line_linked: {
       enable_auto: true,
-      distance: 140,
+      distance: 100,
       color: '#21d0f5',
       opacity: 1,
       width: 1,
@@ -35,7 +35,7 @@ particlesJS('particles-js', {
   interactivity: {
     enable: true,
     mouse: {
-      distance: 250
+      distance: 300
     },
     detect_on: 'canvas', // "canvas" or "window"
     mode: 'grab', // "grab" of false
